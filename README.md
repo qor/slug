d250a8e7c6fa744d2eb86f53ded261f7fc68e218
Hui <junhuif@gmail.com>
2015-09-06 17:53:03 +0800

    Init to add basic files

---
+++ README.md
@@ -0,0 +1,4 @@
+Slug
+====
+
+Slug package is an extension for qor. It provides an easy way to create a pretty URL for your model.
---
+++ slug.go
@@ -0,0 +1,9 @@
+package slug
+
+import "github.com/qor/qor/admin"
+
+type Slug struct {
+}
+
+func (s *Slug) InjectQorAdmin(res *admin.Resource) {
+}
