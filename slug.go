d24343eb08c8d5a521081fa51ebb0a2619f372a9
Jinzhu <wosmvp@gmail.com>
2015-12-02 15:04:58 +0800

    Using configure resource meta API

--- slug.go
+++ slug.go
@@ -4,7 +4,6 @@ import (
 	"database/sql/driver"
 	"os"
 	"path"
-	"reflect"
 	"strings"
 
 	"github.com/qor/qor"
@@ -35,54 +34,42 @@ func (slug Slug) Value() (driver.Value, error) {
 
 var injected bool
 
-func (Slug) ConfigureQorResource(res resource.Resourcer) {
-	if res, ok := res.(*admin.Resource); ok {
-		Admin := res.GetAdmin()
-		scope := Admin.Config.DB.NewScope(res.Value)
+func (Slug) ConfigureQorMeta(meta resource.Metaor) {
+	if meta, ok := meta.(*admin.Meta); ok {
+		res := meta.GetBaseResource().(*admin.Resource)
 
-		if !injected {
-			injected = true
-			for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
-				admin.RegisterViewPath(path.Join(gopath, "src/github.com/qor/slug/views"))
-			}
-			res.UseTheme("slug")
+		for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
+			admin.RegisterViewPath(path.Join(gopath, "src/github.com/qor/slug/views"))
+		}
+		res.UseTheme("slug")
+
+		name := strings.TrimSuffix(meta.Name, "WithSlug")
+		if meta := res.GetMeta(name); meta != nil {
+			meta.Type = "slug"
+		} else {
+			res.Meta(&admin.Meta{Name: name, Type: "slug"})
 		}
 
-		for _, field := range scope.Fields() {
-			if field.Struct.Type == reflect.TypeOf(Slug{}) {
-				name := strings.TrimSuffix(field.Name, "WithSlug")
-
-				if meta := res.GetMeta(name); meta != nil {
-					meta.Type = "slug"
-				} else {
-					res.Meta(&admin.Meta{Name: name, Type: "slug"})
+		var fieldName = meta.Name
+		res.AddValidator(func(record interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
+			if meta := metaValues.Get(fieldName); meta != nil {
+				slug := utils.ToString(metaValues.Get(fieldName).Value)
+				if slug == "" {
+					return validations.NewError(record, fieldName, name+"'s slug can't be blank")
+				} else if strings.Contains(slug, " ") {
+					return validations.NewError(record, fieldName, name+"'s slug can't contains blank string")
 				}
-				if slugMeta := res.GetMeta(field.Name); slugMeta == nil {
-					res.Meta(&admin.Meta{Name: field.Name, Type: "string"})
+			} else {
+				if field, ok := context.GetDB().NewScope(record).FieldByName(meta.Name); ok && field.IsBlank {
+					return validations.NewError(record, fieldName, name+"'s slug can't be blank")
 				}
-
-				var fieldName = field.Name
-				res.AddValidator(func(record interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
-					if meta := metaValues.Get(fieldName); meta != nil {
-						slug := utils.ToString(metaValues.Get(fieldName).Value)
-						if slug == "" {
-							return validations.NewError(record, fieldName, name+"'s slug can't be blank")
-						} else if strings.Contains(slug, " ") {
-							return validations.NewError(record, fieldName, name+"'s slug can't contains blank string")
-						}
-					} else {
-						if field, ok := context.GetDB().NewScope(record).FieldByName(field.Name); ok && field.IsBlank {
-							return validations.NewError(record, fieldName, name+"'s slug can't be blank")
-						}
-					}
-					return nil
-				})
-
-				res.IndexAttrs(append(res.IndexAttrs(), "-"+field.Name)...)
-				res.ShowAttrs(append(res.ShowAttrs(), "-"+field.Name)...)
-				res.EditAttrs(append(res.EditAttrs(), "-"+field.Name)...)
-				res.NewAttrs(append(res.NewAttrs(), "-"+field.Name)...)
 			}
-		}
+			return nil
+		})
+
+		res.IndexAttrs(append(res.IndexAttrs(), "-"+meta.Name)...)
+		res.ShowAttrs(append(res.ShowAttrs(), "-"+meta.Name)...)
+		res.EditAttrs(append(res.EditAttrs(), "-"+meta.Name)...)
+		res.NewAttrs(append(res.NewAttrs(), "-"+meta.Name)...)
 	}
 }
